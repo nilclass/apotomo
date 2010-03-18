4d69bcdd181ef092607fe66054c0f4696633fbe8
niklas <niklas@brueckenschlaeger.de>
2010-03-18 13:53:00 +0100

    minor changes: * StatefulWidget#initialize can handle a block by default * link_to_event passes the :with option on to link_to_remote

--- app/cells/apotomo/stateful_widget.rb
+++ app/cells/apotomo/stateful_widget.rb
@@ -2,33 +2,33 @@ require 'onfire'
 
 module Apotomo
   class StatefulWidget < Cell::Base
-    
+
     class_inheritable_array :initialize_hooks, :instance_writer => false
     self.initialize_hooks = []
-    
+
     attr_accessor :opts
     attr_writer   :visible
-    
+
     include TreeNode
     include Persistence
-    
+
     include Onfire
-    
+
     include EventMethods
     include Transition
     include Caching
-    
+
     include DeepLinkMethods
-    
+
     helper Apotomo::Rails::ViewHelper
-    
-    
+
+
     attr_writer   :controller
     attr_accessor :version
-    
-        
+
+
     # Constructor which needs a unique id for the widget and one or multiple start states.
-    # <tt>start_state</tt> may be a symbol or an array of symbols.    
+    # <tt>start_state</tt> may be a symbol or an array of symbols.
     def initialize(id, start_state, opts={})
       @opts         = opts
       @name         = id
@@ -36,30 +36,32 @@ module Apotomo
 
       @visible      = true
       @version      = 0
-      
+
       @cell         = self
-      
+
       process_initialize_hooks(id, start_state, opts)
+
+      yield(self) if block_given?
     end
-    
+
     def process_initialize_hooks(*args)
       self.class.initialize_hooks.each { |h| send(h, *args) }
     end
-    
+
     def last_state
       @state_name
     end
-    
+
     def visible?
       @visible
     end
 
-    # Defines the instance vars that should <em>not</em> survive between requests, 
+    # Defines the instance vars that should <em>not</em> survive between requests,
     # which means they're not frozen in Apotomo::StatefulWidget#freeze.
     def ivars_to_forget
       unfreezable_ivars
     end
-    
+
     def unfreezable_ivars
       ['@childrenHash', '@children', '@parent', '@controller', '@cell', '@invoke_block', '@ivars_before', '@rendered_children', '@page_updates', '@opts']
     end
@@ -69,32 +71,32 @@ module Apotomo
     def ivars_to_ignore
       []
     end
-    
-    
+
+
     # Returns the rendered content for the widget by running the state method for <tt>state</tt>.
     # This might lead us to some other state since the state method could call #jump_to_state.
     def invoke(state=nil, &block)
       @invoke_block = block ### DISCUSS: store block so we don't have to pass it 10 times?
       logger.debug "\ninvoke on #{name} with #{state.inspect}"
-      
+
       if state.blank?
         state = next_state_for(last_state) || @start_state
       end
-      
+
       logger.debug "#{name}: transition: #{last_state} to #{state}"
       logger.debug "                                    ...#{state}"
-      
+
       render_state(state)
     end
-    
-    
-    
+
+
+
     # called in Cell::Base#render_state
     def dispatch_state(state)
       send(state, &@invoke_block)
     end
-    
-    
+
+
     # Render the view for the current state. Usually called at the end of a state method.
     #
     # ==== Options
@@ -116,11 +118,11 @@ module Apotomo
     #  class MouseCell < Apotomo::StatefulWidget
     #    def eating
     #      # ... do something
-    #      render 
+    #      render
     #    end
     #
     # will just render the view <tt>eating.html</tt>.
-    # 
+    #
     #    def eating
     #      # ... do something
     #      render :view => :bored, :layout => "metal"
@@ -144,43 +146,43 @@ module Apotomo
       if options[:text] or options[:replace_html] # per default, disable framing for :text/:replace_html
         options.reverse_merge!(:frame => false)
       end
-      
+
       options.reverse_merge!  :render_children  => true,
                               :frame            => :div,
                               :html_options     => {},
                               :locals           => {},
                               :replace_html     => false,
                               :invoke           => {}
-                              
-      
+
+
       rendered_children = render_children_for(options)
-      
+
       options[:html_options].reverse_merge!(:id => name)
       options[:locals].reverse_merge!(:rendered_children => rendered_children)
-      
+
       @controller = controller # that dependency SUCKS.
-      
-      
+
+
       ### DISCUSS: provide a better JS abstraction API and de-coupled helpers like #visual_effect.
       if content = options[:js]
         return ::Apotomo::Content::Javascript.new(content)
       end
-      
+
       if content = options[:raw]
         return ::Apotomo::Content::Raw.new(content)
       end
-      
+
       if options[:nothing]
-        return "" 
+        return ""
       end
-      
-      
+
+
       content = render_view_for(options, @state_name) # defined in Cell::Base.
       content = frame_content_for(content, options)
-      
+
       page_update_for(content, options[:replace_html])
     end
-    
+
     def page_update_for(content, replace_html)
       mode = replace_html ? :replace_html : :replace
       ::Apotomo::Content::PageUpdate.new(mode => name, :with => content)
@@ -189,38 +191,38 @@ module Apotomo
     # Wrap the content into a div frame.
     def frame_content_for(content, options)
       return content unless options[:frame]
-      
+
       ### TODO: i'd love to see a real API for helpers in rails.
       Object.new.extend(ActionView::Helpers::TagHelper).content_tag(options[:frame], content, options[:html_options])
     end
-    
 
-    # Force the FSM to go into <tt>state</tt>, regardless whether it's a valid 
+
+    # Force the FSM to go into <tt>state</tt>, regardless whether it's a valid
     # transition or not.
     ### TODO: document the need for return.
     def jump_to_state(state)
       logger.debug "STATE JUMP! to #{state}"
-      
+
       render_state(state)
     end
-    
-    
+
+
     def visible_children
       children.find_all { |kid| kid.visible? }
     end
 
     def render_children_for(options)
       return {} unless options[:render_children]
-      
+
       render_children(options[:invoke])
     end
-    
+
     def render_children(invoke_options={})
       returning rendered_children = ActiveSupport::OrderedHash.new do
         visible_children.each do |kid|
           child_state = decide_state_for(kid, invoke_options)
           logger.debug "    #{kid.name} -> #{child_state}"
-          
+
           rendered_children[kid.name] = render_child(kid, child_state)
         end
       end
@@ -233,14 +235,14 @@ module Apotomo
     def decide_state_for(child, invoke_options)
       invoke_options.stringify_keys[child.name.to_s]
     end
-    
-    
+
+
     ### DISCUSS: use #param only for accessing request data.
     def param(name)
       params[name]
     end
-    
-    
+
+
     # Returns the address hash to the event controller and the targeted widget.
     #
     # Reserved options for <tt>way</tt>:
@@ -248,27 +250,27 @@ module Apotomo
     #             The default is to take the current widget as source.
     #   :type     specifies the event type.
     #
-    # Any other option will be directly passed into the address hash and is 
+    # Any other option will be directly passed into the address hash and is
     # available via StatefulWidget#param in the widget.
     #
     # Can be passed to #url_for.
-    # 
+    #
     # Example:
     #   address_for_event :type => :squeak, :volume => 9
     # will result in an address that triggers a <tt>:click</tt> event from the current
     # widget and also provides the parameter <tt>:item_id</tt>.
     def address_for_event(options)
       raise "please specify the event :type" unless options[:type]
-      
+
       options[:source] ||= self.name
       options
     end
-    
+
     # Returns the widget named <tt>widget_id</tt> as long as it is below self or self itself.
     def find_widget(widget_id)
       find {|node| node.name.to_s == widget_id.to_s}
     end
-    
+
     def controller
       isRoot? ? @controller : root.controller
     end
--- lib/apotomo/rails/view_helper.rb
+++ lib/apotomo/rails/view_helper.rb
@@ -6,22 +6,23 @@ module Apotomo
       # Internally this method just uses <tt>remote_function</tt> for JS output.
       #
       # Example:
-      # 
+      #
       #   <%= image_tag "cheese.png", :onMouseover => trigger_event(:mouseAlarm) %>
       #
       # will trigger the event <tt>:mouseAlarm</tt> when moving the mouse over the cheese image.
       def trigger_event(type, options={})
         remote_function(:url => @controller.compute_event_address_for(@cell, type, options))
       end
-      
+
       # Creates a link that triggers an event via AJAX.
       # This link will <em>only</em> work in JavaScript-able browsers.
       #
       # Note that the link is created using #link_to_remote.
       def link_to_event(title, type, options={}, html_options={})
-        link_to_remote(title, {:url => @controller.compute_event_address_for(@cell, type, options)}, html_options)
+        with = options.delete(:with)
+        link_to_remote(title, {:url => @controller.compute_event_address_for(@cell, type, options), :with => with}, html_options)
       end
-      
+
       # Creates a form tag that triggers an event via AJAX when submitted.
       # See StatefulWidget::address_for_event for options.
       #
@@ -29,21 +30,21 @@ module Apotomo
       def form_to_event(type, options={}, html_options={}, &block)
         form_remote_tag({:url => @controller.compute_event_address_for(@cell, type, options), :html => html_options}, &block)
       end
-      
+
       def url_for_event(type, options={})
         @controller.compute_event_address_for(@cell, type, options)
       end
-      
+
       ### TODO: test me.
       def update_url(fragment)
         'SWFAddress.setValue("' + fragment + '");'
       end
-      
+
       ### TODO: test me.
       ### DISCUSS: rename to rendered_children ?
       def content
         @rendered_children.collect{|e| e.last}.join("\n")
       end
-    end  
+    end
   end
 end
